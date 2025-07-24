fb723ef474aa1d230b466d6df50b369a6e2ac52e
Eli Fife <fife.eli@icloud.com>
2025-07-24 16:29:45 -0400

    Initial Commit

---
+++ EliYouAreAwesome/EliYouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliYouAreAwesomeApp.swift
+//  EliYouAreAwesome
+//
+//  Created by Eli Fife on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EliYouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
