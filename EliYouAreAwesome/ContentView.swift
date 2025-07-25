a0a4c957c6b4e8e2ef8877bff54e447071a14311
Eli Fife <fife.eli@icloud.com>
2025-07-25 12:12:57 -0400

    change to what it needed to be

--- EliYouAreAwesome/ContentView.swift
+++ EliYouAreAwesome/ContentView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+                Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                    .foregroundStyle(.orange)
+                Text("You are awesome!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            }
         .padding()
     }
 }
